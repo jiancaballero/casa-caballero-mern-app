a6f0dd251f5ee8925ef0b240b448294fd12427bd
jiancaballero <jiancaballero@gmail.com>
2022-10-01 21:39:32 +0900

    fix: datepicker disables and popups

--- casa-caballero/src/components/CheckAvailability.js
+++ casa-caballero/src/components/CheckAvailability.js
@@ -6,42 +6,56 @@ import { createSearchParams, useNavigate } from "react-router-dom";
 const CheckAvailability = () => {
   const [checkInDate, setCheckInDate] = useState(moment());
   const [checkOutDate, setCheckOutDate] = useState(moment().add(1, "days"));
-  const [checkOutOpen,setCheckOutOpen] = useState(false)
+  const [checkOutOpen, setCheckOutOpen] = useState(false);
   // const [numberOfNights, setNumberOfNights] = useState(0);
   const getCheckIn = (date) => {
     const checkInCopy = moment(date).clone();
     setCheckInDate(date);
     setCheckOutDate(checkInCopy.add(1, "days"));
-    setCheckOutOpen(true)
+   
   };
   const getCheckOut = (date) => {
-    setCheckOutOpen(false)
+    setCheckOutDate(date);
   };
   const navigate = useNavigate();
-  const params = {
-    checkIn: checkInDate.format("YYYY-MM-DD"),
-    checkOut: checkOutDate.format("YYYY-MM-DD"),
-    adult: 1,
-    child: 0,
-  };
-  const searchRooms = () =>
+
+  const searchRooms = () =>{
+    const params = {
+      checkIn: checkInDate.format("YYYY-MM-DD"),
+      checkOut: checkOutDate.format("YYYY-MM-DD"),
+      adult: 1,
+      child: 0,
+    };
     navigate({
       pathname: "/search",
       search: `?${createSearchParams(params)}`,
     });
+  }
+  
+  
   // const computeNumberOfNights = ()=>{
   //   setNumberOfNights(checkOutDate.diff(checkInDate,'days'))
   // }
 
   const disabledCheckInDate = (current) => {
     // Can not select days before today
-    return moment().add(-1, "days") >= current;
+    return current < moment().endOf('day');
   };
 
   const disabledCheckOutDate = (current) => {
-    // Can not select days before today
-    return current && current < checkInDate.endOf("day");
+    // Can not select days before checkInDate
+    return current && current <= checkInDate.endOf("day");
   };
+
+  const handleCheckInOpen = (open)=>{
+    console.log(open)
+    if (!open) {
+      setCheckOutOpen(true);
+    }
+  }
+  const handleCheckOutOpen = (open)=>{
+    setCheckOutOpen(open);
+  }
   return (
     <>
       <DatePicker
@@ -51,16 +65,22 @@ const CheckAvailability = () => {
         disabledDate={disabledCheckInDate}
         allowClear={false}
         onChange={getCheckIn}
+        onOpenChange={handleCheckInOpen}
+        showToday={false}
+      
+        
       />
 
       <DatePicker
         value={checkOutDate}
         format={"dddd, MMM DD, YYYY"}
         disabledDate={disabledCheckOutDate}
-        allowClear={false}
+        allowClear={true}
         onChange={getCheckOut}
         open={checkOutOpen}
-        
+        onOpenChange={handleCheckOutOpen}
+        showToday={false}
+       
       />
       <Button type="primary" size="large" onClick={searchRooms}>
         Check Availability
