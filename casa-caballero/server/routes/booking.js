8463c45dfa701e7ba8831dba297e8033ffb076a5
jiancaballero <jiancaballero@gmail.com>
2022-11-04 13:09:18 +0800

    for deployment

--- casa-caballero/server/routes/booking.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/booking.controller");
-router.post("/", controller.addBooking);
-router.get("/:bk_code", controller.getBooking);
-router.put("/cancel", controller.cancelBooking);
-module.exports = router;
