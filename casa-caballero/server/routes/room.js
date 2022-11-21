821e5710f7bb0bb8c63c5b86a76c83e9adbd5b9b
jiancaballero <jiancaballero@gmail.com>
2022-11-21 19:51:35 +0900

    fix: deployment

--- casa-caballero/server/routes/room.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/room.controller");
-
-// HTTP METHODS FOR ROOM
-router.post("/", controller.getAvailableRooms);
-
-module.exports = router;
