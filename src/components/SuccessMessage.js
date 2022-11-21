0dcf52df51ad92a3c4ac9dcce86d526d9862669a
jiancaballero <jiancaballero@gmail.com>
2022-11-21 19:47:45 +0900

    fix minor issues

--- src/components/SuccessMessage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { Alert } from "antd";
-const SuccessMessage = ({ message }) => {
-  return <Alert message={message} type="success" showIcon />;
-};
-
-export default SuccessMessage;
